6e4ea5848671e582632886885e57b6d52363b3d9
Elithon Silva <elithonsilva@gmail.com>
2021-09-14 17:07:53 -0300

    1. Crie as rotas necessárias para a aplicação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <main>
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/search" component={ Search } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
           <p>TrybeTunes</p>
         </main>
       </BrowserRouter>
