e36ca6d5c651e48af737a15431862733a9a59fe7
Elithon Silva <elithonsilva@gmail.com>
2021-09-14 17:43:34 -0300

    cria form com input name e button type submit no componente Login

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="/profile" component={ Profile } />
-          <Route path="/" component={ Login } />
+          <Route exact path="/" component={ Login } />
           <Route path="*" component={ NotFound } />
           <p>TrybeTunes</p>
         </main>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="login-name-input">
+            Name:
+            <input type="text" data-testid="login-name-input" />
+          </label>
+          <button type="submit" data-testid="login-submit-button">Entrar</button>
+        </form>
+      </div>
     );
   }
 }
