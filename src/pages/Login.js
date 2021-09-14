0bb8922b20596c675990252c0487dd103599fdfa
Elithon Silva <elithonsilva@gmail.com>
2021-09-14 18:20:32 -0300

    impl: habilita button quando input receber mais que 3 caracteres

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      statusButton: true,
+    };
+  }
+
+  statusButton = (event) => {
+    const minNumber = 3;
+    this.setState({
+      statusButton: event.target.value.length < minNumber,
+    });
+  }
+
   render() {
+    const { statusButton } = this.state;
     return (
       <div data-testid="page-login">
         <form>
           <label htmlFor="login-name-input">
             Name:
-            <input type="text" data-testid="login-name-input" />
+            <input
+              type="text"
+              data-testid="login-name-input"
+              onChange={ this.statusButton }
+            />
           </label>
-          <button type="submit" data-testid="login-submit-button">Entrar</button>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ statusButton }
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
