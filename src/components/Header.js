cdf1a45f0444fcbcc6fc36eba5e0e25d8e529c20
Elithon Silva <elithonsilva@gmail.com>
2021-09-15 14:51:42 -0300

    impl: chamada da funçao getUser do componente userAPI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getUserName();
+  }
+
+  getUserName = async () => {
+    const getFunctionAPI = await getUser();
+    console.log(getFunctionAPI);
+    this.setState({
+      userName: getFunctionAPI.name,
+    });
+  }
+
   render() {
+    const { userName } = this.state;
     return (
-      <header data-testid="header-component" />
+      <header data-testid="header-component">
+        <p data-testid="header-user-name">{ userName }</p>
+      </header>
     );
   }
 }
