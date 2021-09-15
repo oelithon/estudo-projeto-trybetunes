2d37a1aeb021fd228bc89f35050b4c24c998a3ec
Elithon Silva <elithonsilva@gmail.com>
2021-09-15 20:36:25 -0300

    exibe nome do arquista pesquisado

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,8 +24,6 @@ class Header extends Component {
     const getFunctionAPI = await getUser();
     this.setState({
       userName: getFunctionAPI.name,
-    });
-    this.setState({
       loading: false,
     });
   }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
@@ -7,6 +9,9 @@ class Search extends Component {
 
     this.state = ({
       statusButton: true,
+      inputValue: '',
+      artistName: '',
+      loading: false,
     });
   }
 
@@ -14,11 +19,25 @@ class Search extends Component {
     const minNumber = 2;
     this.setState({
       statusButton: event.target.value.length < minNumber,
+      inputValue: event.target.value,
+    });
+  }
+
+  searchButton = async () => {
+    const { inputValue } = this.state;
+    this.setState((prevState) => ({
+      artistName: prevState.inputValue,
+      inputValue: '',
+      loading: true,
+    }));
+    const getAPISearchMusic = await searchAlbumsAPI(inputValue);
+    this.setState({
+      loading: false,
     });
   }
 
   render() {
-    const { statusButton } = this.state;
+    const { statusButton, inputValue, loading, artistName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -27,17 +46,28 @@ class Search extends Component {
             <input
               type="text"
               data-testid="search-artist-input"
+              value={ inputValue }
               onChange={ this.statusButtonFunction }
             />
           </label>
           <button
-            type="submit"
+            type="button"
             data-testid="search-artist-button"
             disabled={ statusButton }
+            onClick={ this.searchButton }
           >
             Pesquisar
           </button>
         </form>
+        {loading
+          ? <Loading />
+          : (
+            <p>
+              Resultado de álbuns de:
+              {' '}
+              {artistName}
+            </p>
+          ) }
       </div>
     );
   }
