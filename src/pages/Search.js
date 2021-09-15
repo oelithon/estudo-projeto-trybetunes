296c4b8c33f7da99198425b3a5d53e386db53554
Elithon Silva <elithonsilva@gmail.com>
2021-09-15 15:35:50 -0300

    impl: habilitacao o botao, quando input for maior que 2

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = ({
+      statusButton: true,
+    });
+  }
+
+  statusButtonFunction = (event) => {
+    const minNumber = 2;
+    this.setState({
+      statusButton: event.target.value.length < minNumber,
+    });
+  }
+
   render() {
+    const { statusButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <form>
           <label htmlFor="search-artist-input">
-            <input type="text" data-testid="search-artist-input" />
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.statusButtonFunction }
+            />
           </label>
           <button
             type="submit"
             data-testid="search-artist-button"
+            disabled={ statusButton }
           >
             Pesquisar
           </button>
